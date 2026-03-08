6e1d7a67ab6fc469aac7230d6bf1bc893f47889a
Rickson Rocha <ricksonroccha21@gmail.com>
2026-03-08 12:36:31 -0300

    git commit -m "fix: remove dist from git tracking"

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,18 +1,12 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 function createPrismaClient(): PrismaClient {
   const connectionString = process.env.DATABASE_URL;
-
-  console.log("Connecting to:", connectionString);
-
   const adapter = new PrismaPg({ connectionString });
-  return new PrismaClient({
-    adapter,
-    log: ["query", "error", "warn"],
-  });
+  return new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
